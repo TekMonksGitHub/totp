ca5d78a80aaae21e08c7913fbf3321b2f288f1c2
Rohit Kapoor <rvkapoor@tekmonks.com>
2020-07-29 10:16:55 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Totp
 TOTP Library for Node.js. Supports various key lengths, HMAC algorithms, durations etc.
 
-
+For documentation please click [here](./docs/global.html).
---
+++ totpTest.js
@@ -0,0 +1,25 @@
+const totp = require("./totp.js");
+const qrcode = require("qrcode");
+const fs = require("fs");
+
+main();
+
+
+function main() {
+    const secret = totp.getSecret();
+    console.log(secret);
+
+    const args = process.argv.slice(2);
+    const token = totp.getTOTP(args[0]||secret);
+    console.log(token);
+
+    const tokenValidates = totp.verifyTOTP(args[0]||secret, token);
+    console.log(tokenValidates);
+
+    const toptURL = totp.getTOTPURL("Teleworkr", args[0]||secret);
+    console.log(toptURL);
+    qrcode.toDataURL(toptURL, function(_, data_url) {
+        console.log(data_url);
+        fs.writeFileSync("qrcode.html", `<img src="${data_url}">`);
+    });
+}
